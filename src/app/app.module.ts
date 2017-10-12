04b79376eabab155fb16e4132455b2b3bd936874
IllyaVolkov <volkov.ilya.1998@gmail.com>
2017-10-12 10:25:06 +0300

    addid saving file in png

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SceneComponent } from './scene/scene.component';
+import { ControlsComponent } from './scene/controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SceneComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
